50fab0cfbe05cbe779327f3a6d2ff849d538d7f4
Siya <siyagoyal629@gmail.com>
2023-07-17 11:17:05 -0400

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Siya Goyal on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
